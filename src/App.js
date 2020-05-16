183751c027b6f807833d40351788f253060e8473
dbinkele <dbinkele@itemis.de>
2020-05-16 06:54:03 +0200

    react experiments

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/MyComponents';
-import ResultTable from './components/ResultTable';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react {process.env.NODE_ENV}
-        </a>
-        <MyComponent />
-        <ResultTable />
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TopMenu from '../components/TopMenu';
+import SideMenu from '../components/SideMenu';
+import Footer from '../components/Footer';
+import MainContent from '../components/MainContent';
+import MyComponent from '../components/MyComponents';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+}));
+
+function Home() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <TopMenu />
+            <SideMenu />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
